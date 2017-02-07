9f2cc6e26e1b0cbba5c693262231a1ecc681c6ab
Mark Jannenga <jannengm@mail.gvsu.edu>
2017-02-07 14:18:49 -0500

    Changed 2 parameters and got 89% accuracy

--- basic_sentiment_pipeline.py
+++ basic_sentiment_pipeline.py
@@ -10,9 +10,10 @@ import pickle
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
+import sys
 
-# Hint: These are not actually used in the current 
-# pipeline, but would be used in an alternative 
+# Hint: These are not actually used in the current
+# pipeline, but would be used in an alternative
 # tokenizer such as PorterStemming.
 import nltk
 nltk.download('stopwords')
@@ -20,8 +21,8 @@ from nltk.corpus import stopwords
 stop = stopwords.words('english')
 
 """
-    This is a very basic tokenization strategy.  
-    
+    This is a very basic tokenization strategy.
+
     Hint: Perhaps implement others such as PorterStemming
     Hint: Is this even used?  Where would you place it?
 """
@@ -34,11 +35,12 @@ df = pd.read_csv('./training_movie_data.csv')
 # Split your data into training and test sets.
 # Allows you to train the model, and then perform
 # validation to get a sense of performance.
-# 
+#
 # Hint: This might be an area to change the size
-# of your training and test sets for improved 
+# of your training and test sets for improved
 # predictive performance.
-training_size = 40000
+training_size = 35000     # 34129 optimal for some reason
+# training_size = int(sys.argv[1])
 X_train = df.loc[:training_size, 'review'].values
 y_train = df.loc[:training_size, 'sentiment'].values
 X_test = df.loc[training_size:, 'review'].values
@@ -46,13 +48,13 @@ y_test = df.loc[training_size:, 'sentiment'].values
 
 # Perform feature extraction on the text.
 # Hint: Perhaps there are different preprocessors to
-# test?  
+# test?
 tfidf = TfidfVectorizer(strip_accents=None,
                         lowercase=False,
                         preprocessor=None)
 
 # Hint: There are methods to perform parameter sweeps to find the
-# best combination of parameters.  Look towards GridSearchCV in 
+# best combination of parameters.  Look towards GridSearchCV in
 # sklearn or other model selection strategies.
 
 # Create a pipeline to vectorize the data and then perform regression.
@@ -60,13 +62,13 @@ tfidf = TfidfVectorizer(strip_accents=None,
 # Look to documentation on Regression or similar methods for hints.
 # Possibly investigate alternative classifiers for text/sentiment.
 lr_tfidf = Pipeline([('vect', tfidf),
-                     ('clf', LogisticRegression(C=0.001,fit_intercept=False,penalty='l1',random_state=0))])
+                     ('clf', LogisticRegression(C=3.900,fit_intercept=False,penalty='l1',random_state=0))])
 
 # Train the pipline using the training set.
 lr_tfidf.fit(X_train, y_train)
 
 # Print the Test Accuracy
-print('Test Accuracy: %.3f' % lr_tfidf.score(X_test, y_test))
+print('Test Accuracy: %.4f' % lr_tfidf.score(X_test, y_test))
 
 # Save the classifier for use later.
 pickle.dump(lr_tfidf, open("saved_model.sav", 'wb'))
